7b1189f2be6b45447979cf15fa6bf59f58801907
Andi Rustianto <anditodev46@gmail.com>
2022-08-01 00:33:23 +0700

    restructure folder project

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import React from 'react';
+import {Home} from '../screen';
+const Stack = createNativeStackNavigator();
+
+const Routes = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Routes;
+
+const styles = StyleSheet.create({});
